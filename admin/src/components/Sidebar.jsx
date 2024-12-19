d19e8b366ae515756250f2ca4886e20f3036ded8
danielecapano <daniele.capano@gmail.com>
2024-12-19 20:23:53 +0100

    FIX - translate admin panel

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -10,21 +10,21 @@ const Sidebar = () => {
           className='flex items-center gap-3 border  border-r-0 px-3 py-2 rounded-l transition-all duration-500 hover:bg-gray-100'
         >
           <img src={assets.add_icon} className='size-5' alt='' />
-          <p className='hidden md:block'>Add Products</p>
+          <p className='hidden md:block'>Aggiungi Prodotti</p>
         </NavLink>
         <NavLink
           to='/list'
           className='flex items-center gap-3 border  border-r-0 px-3 py-2 rounded-l transition-all duration-500 hover:bg-gray-100'
         >
-          <img src={assets.order_icon} className='size-5' alt='' />
-          <p className='hidden md:block'>List Products</p>
+          <img src={assets.list_icon} className='size-5' alt='' />
+          <p className='hidden md:block'>Lista Prodotti</p>
         </NavLink>
         <NavLink
           to='/orders'
           className='flex items-center gap-3 border  border-r-0 px-3 py-2 rounded-l transition-all duration-500 hover:bg-gray-100'
         >
           <img src={assets.order_icon} className='size-5' alt='' />
-          <p className='hidden md:block'>Orders</p>
+          <p className='hidden md:block'>Ordini</p>
         </NavLink>
       </div>
     </div>
